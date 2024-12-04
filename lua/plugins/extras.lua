f6ea3ba57a22fac2c06647c90418258943bf4908
May_Si <77016119+MaySi11@users.noreply.github.com>
2024-12-04 13:35:05 +0530

    add extras for formatting,linting and language support

---
+++ lua/plugins/extras.lua
@@ -0,0 +1,22 @@
+return{
+    
+    -- formating
+    { import = "lazyvim.plugins.extras.formatting.prettier" },
+
+    -- languages
+    {import = "lazyvim.plugins.extras.lang.typescript"},
+    { import = "lazyvim.plugins.extras.lang.docker" },
+    { import = "lazyvim.plugins.extras.lang.git" },
+    { import = "lazyvim.plugins.extras.lang.go" },
+    { import = "lazyvim.plugins.extras.lang.java" },
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.lang.markdown" },
+    { import = "lazyvim.plugins.extras.lang.python" },
+    { import = "lazyvim.plugins.extras.lang.tailwind" },
+    { import = "lazyvim.plugins.extras.lang.terraform" },
+    { import = "lazyvim.plugins.extras.lang.toml" },
+    { import = "lazyvim.plugins.extras.lang.yaml" },
+
+    --linting
+    { import = "lazyvim.plugins.extras.linting.eslint" },
+}
