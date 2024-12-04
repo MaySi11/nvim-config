8d29d1fda911e003d99d5f83a4a5af482811b9dc
May_Si <77016119+MaySi11@users.noreply.github.com>
2024-12-04 21:13:32 +0530

    add color highlight for color codes etc...

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,26 +1,37 @@
 return {
 
-    -- competative programming platforms
-    {
-        "kawre/leetcode.nvim",
-        build = ":TSUpdate html",
-        dependencies = {
-            "nvim-telescope/telescope.nvim",
-            "nvim-lua/plenary.nvim", -- required by telescope
-            "MunifTanjim/nui.nvim",
+  -- competative programming platforms
+  {
+    "kawre/leetcode.nvim",
+    build = ":TSUpdate html",
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+      "nvim-lua/plenary.nvim", -- required by telescope
+      "MunifTanjim/nui.nvim",
 
-            -- optional
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-tree/nvim-web-devicons",
-        },
-        opts = {
-            -- configuration goes here
+      -- optional
+      "nvim-treesitter/nvim-treesitter",
+      "nvim-tree/nvim-web-devicons",
+    },
+    opts = {
+      -- configuration goes here
 
-            ---@type string
-            arg = "leet",
+      ---@type string
+      arg = "leet",
 
-            ---@type lc.lang
-            lang = "cpp"
-        },
-    }
+      ---@type lc.lang
+      lang = "cpp",
+    },
+  },
+
+  -- colour highlight support
+  {
+    "NvChad/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = { -- set to setup table
+      user_default_options = {
+        mode = "virtualtext",
+      },
+    },
+  },
 }
