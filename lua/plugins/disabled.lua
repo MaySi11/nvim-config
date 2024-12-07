94ad85cf92a03cee5d498a9e1bb73df6c87af71a
May_Si <77016119+MaySi11@users.noreply.github.com>
2024-12-07 15:14:12 +0530

    Disabled plugins to explore them and what they do

---
+++ lua/plugins/disabled.lua
@@ -0,0 +1,68 @@
+local is_in_use = false
+return {
+
+  -- Coding
+  { "hrsh7th/nvim-cmp", enabled = is_in_use },
+  { "hrsh7th/cmp-nvim-lsp", enabled = is_in_use },
+  { "hrsh7th/cmp-path", enabled = is_in_use },
+  { "hrsh7th/cmp-buffer", enabled = is_in_use },
+  { "garymjr/nvim-snippets", enabled = is_in_use },
+  { "rafamadriz/friendly-snippets", enabled = is_in_use },
+  { "echasnovski/mini.pairs", enabled = is_in_use },
+  { "folke/ts-comments.nvim", enabled = is_in_use },
+  { "echasnovski/mini.ai", enabled = is_in_use },
+  { "folke/lazydev.nvim", enabled = is_in_use },
+
+  -- Editor
+  { "nvim-neo-tree/neo-tree.nvim", enabled = is_in_use },
+  { "MagicDuck/grug-far.nvim", enabled = is_in_use },
+  { "folke/flash.nvim", enabled = is_in_use },
+  { "lewis6991/gitsigns.nvim", enabled = is_in_use },
+  { "folke/trouble.nvim", enabled = is_in_use },
+  { "folke/todo-comments.nvim", enabled = is_in_use },
+
+  -- Formatting
+  { "stevearc/conform.nvim", enabled = is_in_use },
+
+  -- Linting
+  { "mfussenegger/nvim-lint", enabled = is_in_use },
+
+  -- LSP
+  { "neovim/nvim-lspconfig", enabled = is_in_use },
+  { "williamboman/mason-lspconfig.nvim", enabled = is_in_use },
+  { "williamboman/mason.nvim", enabled = is_in_use },
+
+  --TreeSitter
+  { "folke/which-key.nvim", enabled = is_in_use },
+  { "nvim-treesitter/nvim-treesitter", enabled = is_in_use },
+  { "nvim-treesitter/nvim-treesitter-textobjects", enabled = is_in_use },
+  { "windwp/nvim-ts-autotag", enabled = is_in_use },
+
+  -- UI
+  { "akinsho/bufferline.nvim", enabled = is_in_use },
+  { "nvim-lualine/lualine.nvim", enabled = is_in_use },
+  { "lukas-reineke/indent-blankline.nvim", enabled = is_in_use },
+  { "folke/noice.nvim", enabled = is_in_use },
+  { "echasnovski/mini.icons", enabled = is_in_use },
+  { "MunifTanjim/nui.nvim", enabled = is_in_use },
+  --  { "folke/snacks.nvim", enabled = is_in_use },
+
+  --Util
+  { "folke/persistence.nvim", enabled = is_in_use },
+  { "nvim-lua/plenary.nvim", enabled = is_in_use },
+
+  -- Others
+  { "leetcode.nvim", enabled = is_in_use },
+  { "tokyonight.nvim", enabled = is_in_use },
+  { "telescope.nvim", enabled = is_in_use },
+  { "SchemaStore.nvim", enabled = is_in_use },
+  { "nvim-jdtls", enabled = is_in_use },
+  { "nvim-colorizer.lua", enabled = is_in_use },
+  { "dressing.nvim", enabled = is_in_use },
+  { "catppuccin", enabled = is_in_use },
+  { "markdown-preview.nvim", enabled = is_in_use },
+  { "render-markdown.nvim", enabled = is_in_use },
+  { "telescope-terraform.nvim", enabled = is_in_use },
+  { "telescope-terraform-doc.nvim", enabled = is_in_use },
+  { "telescope-fzf-native.nvim", enabled = is_in_use },
+}
