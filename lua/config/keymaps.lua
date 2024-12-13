7a4f37c67bc00229b2f53a1b5703c883b8ea4bc8
May_Si <77016119+MaySi11@users.noreply.github.com>
2024-12-13 16:38:25 +0530

    ADD Disable Arrow Keys

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+local map = vim.keymap.set
+
+-- config toggle variables
+local disable_arrow_keys = true
+
+-- Disable arrow keys for movement
+
+if disable_arrow_keys then
+  local arrow_keys = { "<Up>", "<Down>", "<Left>", "<Right>" }
+  for _, key in ipairs(arrow_keys) do
+    map({ "n", "x" }, key, "<Nop>", { silent = true, desc = "Disable " .. key:gsub("[<>]", "") .. " arrow key" })
+  end
+end
